f5a85d058b1e7245a227389ccc6afc2e8821cf80
thecodingman11 <145624731+thecodingman11@users.noreply.github.com>
2023-12-05 02:40:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 let enterNumber;
-    let currentStreak = 0;
-    let highestStreak = 0;
+let currentStreak = 1;
+let lowestStreak = Infinity;
 
-    function chooseNumber() {
-        enterNumber = parseInt(prompt("Plese enter the maximum value x for the guessing game:"));
-        startGame();
+function chooseNumber() {
+    enterNumber = parseInt(prompt("Please enter the maximum value x for the guessing game:"));
+    startGame();
+}
+
+function startGame() {
+    const randomNumber = Math.floor(Math.random() * enterNumber) + 1;
+    let userGuesses = 0;
+
+    while (userGuesses !== randomNumber) {
+        userGuesses = parseInt(prompt("Guess a number between 1 and " + enterNumber));
+        if (userGuesses === randomNumber) {
+            alert("Congratulations! You guessed the correct number: " + randomNumber + " ," + " in "  + currentStreak + " attempts");
+        } else {
+            alert(`Wrong guess! Try again.`);
+            currentStreak++;
+        }
     }
 
-    function startGame() {
-        const randomNumber = Math.floor(Math.random() * enterNumber) + 1;
-        let userGuesses = parseInt(prompt(`Guess a number between 1 and ${enterNumber}:`));
-
-       // if (userGuesses === randomNumber) {
-       // alert(`You guessed the correct number! Current Streak: ${currentStreak}  Highest Streak: ${highestStreak}`);
-       //   } else {
-        // currentStreak = 0;
-         //  alert(`Sorry, the number was ${randomNumber}. Your streak has been reset.\Highest Streak: ${highestStreak}`);
-       //  }
-       // 
+    if (currentStreak < lowestStreak) {
+        lowestStreak = currentStreak;
+        alert(`New high score! Lowest streak: ${lowestStreak}`);
     }
+    currentStreak = 1; 
+}
