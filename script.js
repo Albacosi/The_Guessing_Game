d3cfcef326ed16e4abb534c416ed838c5142c742
thecodingman11 <145624731+thecodingman11@users.noreply.github.com>
2023-12-05 16:39:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ function startGame() {
     currentStreak = 1; 
 }
 
+
+
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+
+    const isDarkMode = body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+}
+
+
+
+
+
